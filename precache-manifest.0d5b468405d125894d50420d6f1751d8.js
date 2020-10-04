045e90b88ce48099cbb9c0d0cffdd5d47e4519b8
Autunnoo <68438045+Autunnoo@users.noreply.github.com>
2020-10-04 18:06:37 +0300

    Updates

---
+++ precache-manifest.0d5b468405d125894d50420d6f1751d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c4ab9076f8e41f8830c624d9cb2576",
+    "url": "/Redux/index.html"
+  },
+  {
+    "revision": "5c16ac7654b366525bd8",
+    "url": "/Redux/static/css/main.be5d77f7.chunk.css"
+  },
+  {
+    "revision": "ae0361f2976e55dae342",
+    "url": "/Redux/static/js/2.9a1a1227.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux/static/js/2.9a1a1227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c16ac7654b366525bd8",
+    "url": "/Redux/static/js/main.fb843a0c.chunk.js"
+  },
+  {
+    "revision": "43f93f15f06570c223e2",
+    "url": "/Redux/static/js/runtime-main.8825fb20.js"
+  },
+  {
+    "revision": "6254420da120a8adab8583898dcbcd9a",
+    "url": "/Redux/static/media/galochka.6254420d.png"
+  },
+  {
+    "revision": "a37c5d0be56da5d6b3339af384306af0",
+    "url": "/Redux/static/media/like.a37c5d0b.png"
+  },
+  {
+    "revision": "a4881eb9aead2b6f08b22776f238f171",
+    "url": "/Redux/static/media/message.a4881eb9.png"
+  },
+  {
+    "revision": "9761d2ae0838ad7dce8bc607d050ef1b",
+    "url": "/Redux/static/media/repost.9761d2ae.png"
+  }
+]);
